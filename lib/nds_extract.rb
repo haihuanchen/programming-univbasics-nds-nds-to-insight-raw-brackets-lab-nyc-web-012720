ea9e81340ccbb1f024b2abf648178ea9dc407afe
Sam Chen <haihuan.chen@hotmail.com>
2020-01-23 23:56:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,12 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+hash = {
+  "1st Director's Name"=>1234567890,
+  "2nd Director's Name"=>1234577890,
+  "3rd Director's Name"=>1234709136,
+  ...
+}
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
