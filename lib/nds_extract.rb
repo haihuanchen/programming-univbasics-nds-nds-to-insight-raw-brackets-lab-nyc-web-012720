b897dbd9c64c87a740de22f5e889a9d1c7f0813a
Sam Chen <haihuan.chen@hotmail.com>
2020-01-23 23:57:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,5 +11,24 @@ def directors_totals(nds)
   require 'pp'
   pp nds
   
-  
+  grand_total = 0
+row_index = 0
+while row_index < vm.length do
+  column_index = 0
+  while column_index < vm[row_index].length do
+    inner_len = vm[row_index][column_index].length
+    inner_index = 0
+    while inner_index < inner_len do
+      # Explanation!
+      # vm[row][column][spinner]
+      # spinner is full of Hashes with keys :price and :name
+      grand_total += vm[row_index][column_index][inner_index][:price]
+      inner_index += 1
+    end
+    column_index += 1
+  end
+  row_index += 1
+end
+ 
+p grand_total #=> 1192
 end
